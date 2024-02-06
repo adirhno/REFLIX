01a9e6fed8dc26bde4edcf7d96f802a32d343be1
adirhno <adirhno@gmail.com>
2024-02-06 17:45:01 +0200

    final commit

--- src/App.js
+++ src/App.js
@@ -1,120 +1,56 @@
 /** @format */
 
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  useParams,
-  Routes,
-  Link,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Landing from "./components/Landing";
 import Catalog from "./components/Catalog";
 import NavBar from "./components/NavBar";
 import MovieDetail from "./components/MovieDetail";
 import { useEffect, useState } from "react";
-import Rented from "./components/Rented";
+import config from "./config";
 
 function App() {
-  let data = [
-    {
-      id: 0,
-      isRented: false,
-      title: "Tarzan",
-      year: 1999,
-      img: "https://vignette.wikia.nocookie.net/disney-fan-fiction/images/4/42/Tarzan_2004_cover.jpg/revision/latest?cb=20140331030811",
-      descrShort:
-        "Tarzan was born into wealth but raised into incredible misfortune. Shiprweck, parents mauled by a jaguar. Luckily, a troop of gorillas took him in, but the Big Daddy gorilla never took a liking to him. That is, until the end when it's too late. Why is it too late? Watch and find out.",
-    },
-    {
-      id: 1,
-      isRented: false,
-      title: "The Lion King",
-      img: "https://img00.deviantart.net/b782/i/2006/207/e/7/the_lion_king_front_cd_cover_by_peachpocket285.jpg",
-      year: 1994,
-      descrShort:
-        "A young lion prince named Simba is born into wealth but raised into incredible misfortune. Trickster uncle, dying father, usurpation. Luckily, an unlikely meerkat-warthog pair take him in and teach him The Ways of the Bum Life. Be prepared for ghostly hallucinations, wild baboons, creepy crawlies.",
-    },
-    {
-      id: 2,
-      isRented: false,
-      title: "Beauty and the Beast",
-      year: 1991,
-      img: "https://images-na.ssl-images-amazon.com/images/I/81etFyb9N-L._SL1500_.jpg",
-      descrShort:
-        "A kickass woman named Belle who does not succumb to social norms gets crap from a bunch of village idiots, chief amongst them a total tool named Gaston. Belle shows everyone how great she is when she turns a beast (not Gaston) into a man. Love ensues, but then the villagers fall trap to severe group-think mentality led by the main tool himself.",
-    },
-    {
-      id: 3,
-      isRented: false,
-      title: "The Sword in the Stone",
-      year: 1963,
-      img: "https://www.disneyinfo.nl/images/laserdiscs/229-1-AS-front.jpg",
-      descrShort:
-        "Arthur is a young boy who just wants to be a knight's squire. Alas, he is dubbed 'Wart' early on, and it was all downhill from there for a while. On a hunting trip he falls in on Merlin, literally. Merlin is a possibly-mentally-unstable-and-ethically-dubious Wizard that turns Arthur into a literate, at-one-point harassed squirrel. Watch to find out what the heck that means.",
-    },
-    {
-      id: 4,
-      isRented: false,
-      title: "Beauty and the Beast2",
-      year: 2016,
-      img: "https://images-na.ssl-images-amazon.com/images/I/51ArFYSFGJL.jpg",
-      descrShort:
-        "Basically the same as the original, except now Hermi-- Emma Wattson plays Belle, fittingly so some would say, given how actively progressive she is regarding women's rights. Rumor has it that in the bonus scenes she whips out a wand and turns Gaston into a toad, but in order to watch those scenes you need to recite a certain incantation.",
-    },
-  ];
-  let userData = [
-    { name: "Adir", color: "userName gray" },
-    { name: "Reem", color: "userName red" },
-    { name: "Shelly", color: "userName blue" },
-	{ name: "Noy", color: "userName yellow" }
-  ];
-
-  const [movies, setMovies] = useState(data);
+  const [movies, setMovies] = useState(config.data);
   const [budget, setBudget] = useState(10);
-  const [rentedMovies, setRentedMovies] = useState([]);
   const [notRentedMovies, setNotRentedMovies] = useState(movies);
   const [isResults, setIsResults] = useState(false);
   const [results, setResults] = useState([]);
   const [userRentedMovies, setUserRentedMovies] = useState([]);
-  const [rentedTemplate, setRentedTemplate] = useState(false);
   const [input, setInput] = useState("");
   const [user, setUser] = useState("");
   const [template, setTemplate] = useState("landing");
   const [movieDetails, setMovieDetails] = useState("");
 
+  useEffect(() => localStorage.clear(), []);
 
-  useEffect(()=>localStorage.clear(),[])
- 
   const handleMovieDetails = (movieName) => {
-    let details = data.find((m) => m.title === movieName);
+    let details = config.data.find((m) => m.title === movieName);
     setMovieDetails(details);
   };
 
   const handleUser = (userName) => {
-	setTemplate("catalog");
+    setTemplate("catalog");
     setUser(userName);
     if (!localStorage.getItem(userName)) {
-		localStorage.setItem(userName+"budget",10)
-		setBudget(10)
+      localStorage.setItem(userName + "budget", 10);
+      setBudget(10);
       localStorage.setItem(userName, JSON.stringify([]));
       setUserRentedMovies([]);
     } else {
-		let userBudget = JSON.parse(localStorage.getItem(userName+"budget"));
+      let userBudget = JSON.parse(localStorage.getItem(userName + "budget"));
       let moviesFromDb = JSON.parse(localStorage.getItem(userName));
       setUserRentedMovies(moviesFromDb);
-	  setBudget(userBudget)
+      setBudget(userBudget);
     }
   };
 
   const handleRemoveBtn = (removedMovie) => {
     if (isUpdateBudget(3)) {
-		let userMovies=JSON.parse(localStorage.getItem(user))
-		let index=userMovies.findIndex((m)=> m.title === removedMovie.title)
-			userMovies.splice(index,1)
-			localStorage.setItem(user,JSON.stringify(userMovies))
-			setUserRentedMovies(userMovies)
+      let userMovies = JSON.parse(localStorage.getItem(user));
+      let index = userMovies.findIndex((m) => m.title === removedMovie.title);
+      userMovies.splice(index, 1);
+      localStorage.setItem(user, JSON.stringify(userMovies));
+      setUserRentedMovies(userMovies);
       updateMovies(false, removedMovie);
     }
   };
@@ -123,7 +59,6 @@ function App() {
     let moviesFromDb = JSON.parse(localStorage.getItem(user));
     if (isUpdateBudget(-3)) {
       updateMovies(true, movieRented);
-      setRentedTemplate(true);
       moviesFromDb.push({ ...movieRented });
       localStorage.setItem(user, JSON.stringify(moviesFromDb));
       setUserRentedMovies(moviesFromDb);
@@ -139,8 +74,6 @@ function App() {
 
     let newNotRentedMovies = movies.filter((m) => m.isRented === false);
     setNotRentedMovies(newNotRentedMovies);
-    let newRentedMovies = movies.filter((m) => m.isRented === true);
-    setRentedMovies(userRentedMovies);
   };
 
   const filterLetters = (word) => {
@@ -154,8 +87,8 @@ function App() {
       return false;
     } else {
       setBudget((pre) => pre + action);
-	  console.log("the budget is",budget)
-	  localStorage.setItem(user+"budget",JSON.stringify(budget+action))
+      console.log("the budget is", budget);
+      localStorage.setItem(user + "budget", JSON.stringify(budget + action));
       return true;
     }
   };
@@ -172,9 +105,10 @@ function App() {
     });
   };
 
-  return <div className="App">
-	 <NavBar
-	    handleAddBtn={handleAddBtn}
+  return (
+    <div className="App">
+      <NavBar
+        handleAddBtn={handleAddBtn}
         template={template}
         setIsResults={setIsResults}
         budget={budget}
@@ -187,10 +121,17 @@ function App() {
       ></NavBar>
       {!isResults ? (
         <Routes>
-          <Route path={"/"} element={<Landing users={userData}handleUser={handleUser} />}
+          <Route
+            path={"/"}
+            element={
+              <Landing users={config.userData} handleUser={handleUser} />
+            }
           />
-          <Route path={"/catalog"} element={<Catalog
-		  		handleAddBtn={handleAddBtn}
+          <Route
+            path={"/catalog"}
+            element={
+              <Catalog
+                handleAddBtn={handleAddBtn}
                 handleRemoveBtn={handleRemoveBtn}
                 rentedMovies={userRentedMovies}
                 movies={notRentedMovies}
@@ -198,13 +139,21 @@ function App() {
               />
             }
           />
-          <Route path={"/details"} element={<MovieDetail setRentedTemplate={setRentedTemplate} handleAddBtn={handleAddBtn} movieDetails={movieDetails} />}
+          <Route
+            path={"/details"}
+            element={
+              <MovieDetail
+                handleAddBtn={handleAddBtn}
+                movieDetails={movieDetails}
+              />
+            }
           />
         </Routes>
       ) : (
         <></>
       )}
-  </div>;
+    </div>
+  );
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,6 +6,7 @@ export default function Movie({movie, handleMovieDetails, handleAddBtn,handleRem
    <div className='movie'onClick={()=>handleMovieDetails(movie.title)}><Link to={'/details'}> <img src={movie.img}></img></Link>
    {addBtn?<button className='addBtn' onClick={()=>handleAddBtn(movie)}>+</button>:
    <button className='addBtn' onClick={()=>handleRemoveBtn(movie)}>-</button>}
+   {/* <button className='addBtn' onClick={()=>action(movie)}>{ACTION_TO_ICON(action)}</button> */}
     </div>
   )
 }
